5fc5a93380b771c50b8f6a2b3d5d225a72987727
daniel-nelson <daniel.wolcott.nelson@gmail.com>
2015-06-16 13:06:50 -0700

    Cohort analysis methods

--- lib/tracker_jacker/trackable_event.rb
+++ lib/tracker_jacker/trackable_event.rb
@@ -2,5 +2,13 @@ module TrackerJacker
   class TrackableEvent < ActiveRecord::Base
     belongs_to :trackable, polymorphic: true
     belongs_to :owner, polymorphic: true
+    
+    def week_created
+      created_at.at_beginning_of_week
+    end
+
+    def month_created
+      created_at.at_beginning_of_month
+    end
   end
 end
